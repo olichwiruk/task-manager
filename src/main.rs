ad6fd48c79bc3ca139ee4def103ce6a534b17993
Marcin Olichwiruk <21108638+olichwiruk@users.noreply.github.com>
2025-09-21 19:45:15 +0200

    feat: integrate axum web framework and askama templating engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod template;
+
+use axum::{response::IntoResponse, routing::get};
+use template::{HtmlTemplate, IndexTemplate};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let app = axum::Router::new().route("/", get(index));
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await?;
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn index() -> impl IntoResponse {
+    HtmlTemplate(IndexTemplate {})
 }
