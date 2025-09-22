c18447ccf73e2281b925dc420655c01f4ecdf487
Marcin Olichwiruk <21108638+olichwiruk@users.noreply.github.com>
2025-09-22 08:50:43 +0200

    refactor: consolidate templates module into views module

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,7 +1,7 @@
 pub mod task_handler;
 
 use axum::response::IntoResponse;
-use crate::views::templates::{HtmlTemplate, IndexTemplate};
+use crate::views::{HtmlTemplate, IndexTemplate};
 
 pub async fn index() -> impl IntoResponse {
     HtmlTemplate(IndexTemplate {})
